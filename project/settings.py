383b844ec4621c3b2032bbb5b03064e204b8296e
Kenno-Warise <wariken0523@gmail.com>
2021-09-19 09:36:20 +0900

    host set

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r)!v^y&fulvux^nwjc8!4km2+q8ygso%kjap7981a@kjw05+9$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
